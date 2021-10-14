5f35a7ca916816ce49e7f5ab040ed66b552b65e0
binduvukyam <43897505+binduvukyam@users.noreply.github.com>
2021-10-14 15:12:10 -0700

    update truckcreateresponsetesitng

--- src/main/java/com/TruckApi/TruckApi/Model/TruckCreateResponse.java
+++ src/main/java/com/TruckApi/TruckApi/Model/TruckCreateResponse.java
@@ -10,7 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TruckCreateResponse {
-
 	private String status;
 	private String transporterId;
 	private String truckId;
